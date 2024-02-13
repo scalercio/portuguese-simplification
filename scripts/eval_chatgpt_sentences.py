5479a9c9a71ec8807f63fe9e59bfdc3226391da9
Arthur Scalercio <arthurscalercio@gmail.com>
2024-02-12 22:39:14 -0300

    evaluating chatgpt on asset

--- scripts/eval_chatgpt_sentences.py
+++ scripts/eval_chatgpt_sentences.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     one_shot = True
     types = ['sintática','anáfora', 'ordem', 'redundante_lexical']
     model_version = "chatgpt"
-    dataset = 'ccnet'
+    dataset = 'complex.test.asset.txt_'
     config = {
         'prompt': prompt,
         'repeat': repeat,
@@ -43,9 +43,19 @@ if __name__ == '__main__':
         mode = 'disabled',
         config=config            
     )
-    with open(config['evaluate_kwargs']['refs_sents_paths'][0], 'r') as f1, open(config['evaluate_kwargs']['orig_sents_path'], 'r') as f2:
-        src_seq = f2.readlines()
-        ref_seq = f1.readlines()
+    if 'asset' not in dataset:        
+        with open(config['evaluate_kwargs']['refs_sents_paths'][0], 'r') as f1, open(config['evaluate_kwargs']['orig_sents_path'], 'r') as f2:
+            src_seq = f2.readlines()
+            ref_seq = f1.readlines()
+    else:
+        with open(config['evaluate_kwargs']['orig_sents_path'], 'r') as f2:
+            src_seq = f2.readlines()
+            
+        ref_seq = []
+        for i in range(10):
+            with open(f'data/asset/test/simp{i}.txt') as f:
+                ref_seq.append(f.readlines())
+                
     #    src_seq = f2.read().split('\n')
     #    ref_seq = f1.read().split('\n')
     #
@@ -57,22 +67,30 @@ if __name__ == '__main__':
     #for sent in ref_seq:
     #    print(sent)
     print(len(src_seq))
-    assert len(src_seq) == len(ref_seq)
+    if 'asset' in dataset:
+        assert len(src_seq) == len(ref_seq[9])
+    else:
+        assert len(src_seq) == len(ref_seq)
+        
     assert not one_shot == few_shot
     result = 0
     all_sentences = []
-    ref_final = []
+    ref_final = [] if 'asset' not in dataset else [[] for _ in range(10)]
     src_final = []
     for tipo_one_shot in types:
         for i in range(3):
-            ref_final.extend(ref_seq)
+            if 'asset' not in dataset:
+                ref_final.extend(ref_seq)
+            else:
+                for j in range(10):
+                    ref_final[j].extend(ref_seq[j])
             src_final.extend(src_seq)
             if 'feng' in prompt and few_shot:
                 simplified_file = f"data/porsimplessent/chatgpt/few_shot_feng/simplified_gpt-3.5-turbo-instruct_fengetal_few_shot_repete_4few_{i+1}.json"
             elif few_shot:
                 simplified_file = f"data/porsimplessent/chatgpt/few_shot_kew/simplified_gpt-3.5-turbo-instruct_kewetal_few_shot_repete_4few_{i+1}.json"
             elif 'feng' in prompt:
-                simplified_file = f'data/porsimplessent/chatgpt/one_shot_feng/simplified_gpt-3.5-turbo-instruct_fengetal_one_shot_repete_{tipo_one_shot}_{i+1}.json'
+                simplified_file = f'data/asset/chatgpt/one_shot_feng/simplified_gpt-3.5-turbo-instruct_fengetal_one_shot_repete_{tipo_one_shot}_{dataset}{i+1}.json'
             else:
                 simplified_file = f'data/porsimplessent/chatgpt/one_shot_kew/simplified_gpt-3.5-turbo-instruct_kewetal_one_shot_repete_{tipo_one_shot}_{i+1}.json'
 
@@ -84,20 +102,27 @@ if __name__ == '__main__':
                 #print(sentence_dict['simplified'])
                 all_sentences.append(sentence_dict['simplified'])
 
-    assert len(ref_final) == len(all_sentences)
-    assert len(ref_final) == len(src_final)
+    if 'asset' in dataset:
+        assert len(ref_final[9]) == len(all_sentences)
+    else:
+        assert len(ref_final) == len(all_sentences)
+    
+    assert len(all_sentences) == len(src_final)
     print(len(ref_final)/12)
 
     results={}
     
     results['sari'] = corpus_sari(orig_sents=src_final,
                        sys_sents=all_sentences,
-                       refs_sents=[ref_final])
+                       refs_sents=ref_final)
     
     results['bleu'] = corpus_bleu(sys_sents=all_sentences,
-                       refs_sents=[ref_final],
+                       refs_sents=ref_final,
                        lowercase=True)
-    P, R, F1 = score(all_sentences, ref_final, lang = 'pt', verbose = True)
+    if 'asset' in dataset:
+        P, R, F1 = score(all_sentences, list(map(list, zip(*ref_final))), lang = 'pt', verbose = True)
+    else:
+        P, R, F1 = score(all_sentences, ref_final, lang = 'pt', verbose = True)
     results['bert_score'] = F1.mean()
     results['outputs_unchanged'] = get_outputs_unchanged(all_sentences,src_final)
     print(results)
